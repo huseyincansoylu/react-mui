f2e77fe7f55c3a6e759ace73ce573b8de3a89379
hcsoylu <hcsoylu.dev@gmail.com>
2022-10-16 19:37:08 +0300

    Menu

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -1,14 +1,49 @@
-import React from 'react'
-import { AppBar, Toolbar, IconButton } from '@mui/material'
+import React, { useState } from 'react'
+import { AppBar, Toolbar, IconButton, Menu, MenuItem, Button } from '@mui/material'
 import { CatchingPokemon } from '@mui/icons-material'
 
 const MuiNavbar = () => {
+    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const open = Boolean(anchorEl);
+
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <AppBar position='static'>
             <Toolbar>
-                <IconButton size="large" edge="start" color="inherit">
+                <IconButton size="large" edge="start" color="inherit" >
                     <CatchingPokemon />
                 </IconButton>
+                <div>
+                    <Button
+                        id="basic-button"
+                        aria-controls={open ? 'basic-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleClick}
+                        sx={{ color: "white" }}
+                    >
+                        Dashboard
+                    </Button>
+                    <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+                        <MenuItem onClick={handleClose}>Profile</MenuItem>
+                        <MenuItem onClick={handleClose}>My account</MenuItem>
+                        <MenuItem onClick={handleClose}>Logout</MenuItem>
+                    </Menu>
+                </div>
             </Toolbar>
         </AppBar>
     )
